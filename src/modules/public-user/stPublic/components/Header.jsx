352c074f951f1ea8429c7181e40c8e60d0139f03
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2025-01-28 03:27:25 +0000

    added styling to header

--- src/modules/public-user/stPublic/components/Header.jsx
+++ src/modules/public-user/stPublic/components/Header.jsx
@@ -6,17 +6,17 @@ import { FiLogIn } from 'react-icons/fi'
 
 const Header = () => {
     return (
-        <div className="w-full flex flex-col justify-center align-items-center h-[5rem] border-b border-gray-200">
-            <div className=" w-[80%] mx-auto flex items-center justify-between px-4 list-none text-decoration-none">
+        <div className="w-full flex flex-col justify-center align-items-center h-[5.5rem] border-b border-gray-200">
+            <div className=" w-[80%] mx-auto flex items-center justify-between px-4 ">
                 <div className="flex items-center w-[50%] gap-[3rem]">
-                    <img src={LogoImage} alt="Logo" className="w-[8rem] h-[2.5rem] object-contain" />
-                    <nav className="flex gap-[2rem]">
-                        <Link>Home</Link>
+                    <img src={LogoImage} alt="Logo" className="w-[8rem] h-[3rem] object-contain" />
+                    <nav className="flex gap-[2.5rem]">
+                        <Link to={StudentPublicRoutes.home}>Home</Link>
                         <Link>Courses</Link>
                     </nav>
                 </div>
                 <div className="flex items-center gap-4 w-[50%] justify-end">
-                    <Link to={StudentPublicRoutes.login} className="text-primary-black font-semibold border border-primary-black px-[1.5rem] py-[0.5rem] rounded">Login <FiLogIn /></Link>
+                    <Link to={StudentPublicRoutes.login} className="flex items-center gap-[0.5rem] text-[var(--primary-blue)] font-[600] border border-[var(--primary-blue)] px-[2rem] py-[1rem] rounded-[0.3rem]">Login <FiLogIn size='1.5rem' color='var(--primary-blue)'/></Link>
                 </div>
             </div>
         </div>

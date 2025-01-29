042112c38fe501e75979b1300a402107bea2d6b8
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2025-01-29 10:53:04 +0000

    adjusted header height

--- src/modules/public-user/stPublic/sections/HeroSection.jsx
+++ src/modules/public-user/stPublic/sections/HeroSection.jsx
@@ -2,9 +2,9 @@ import HeroImage from '../../../../assets/pc-robo.png'
 
 const HeroSection = () => {
     return (
-        <div className='w-full h-[60.5dvh] bg-[var(--primary-blue)]'>
+        <div className='w-full h-[70dvh] bg-[var(--primary-blue)]'>
             <div className="w-[80%] h-[100%] mx-auto flex items-center justify-between">
-                <div className="flex flex-col justify-start items-start w-[40%] h-[60%] text-[var(--bg-white)]">
+                <div className="flex flex-col justify-start items-start w-[50%] h-[90%] text-[var(--bg-white)]">
                     <h1 className='text-[2.5rem] font-bold'>Unlock Your Potential with 
                     Industry-Leading Courses!</h1>
                     <p className='text-[1.2rem] mt-4'>&quot;Join thousands of learners gaining real-world skills and advancing their careers. Our expert-led courses are designed to empower you to succeed.&quot;</p>

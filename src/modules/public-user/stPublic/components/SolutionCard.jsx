459f6af00b89f998d062bbed030b856d86ae1310
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2025-01-30 03:28:46 +0000

    added styling to solutions cards

--- src/modules/public-user/stPublic/components/SolutionCard.jsx
+++ src/modules/public-user/stPublic/components/SolutionCard.jsx
@@ -2,22 +2,24 @@ import PropTypes from 'prop-types'
 
 const SolutionCard = ({logo, title, description, price}) => {
     return (
-        <div className="flex flex-col items-start justify-center gap-[1rem] box-shadow: 0px 16px 32px 0px #00000026;
-">
+        <div className={`flex flex-col items-start justify-center gap-[1rem] shadow-[0px_16px_32px_0px_#00000026] box-border max-w-[30%] h-[100%] rounded-[1rem] py-[0.5rem] px-[2rem]`}>
             {logo &&<img src={logo} alt="Software Development Logo" className="w-[5rem] h-[5rem] object-contain" />}
             <h3>{title}</h3>
             <p>{description}</p>
-            <p>Price: {price}</p>
+            <div className='flex items-center gap-[0.5rem]'>
+                <p>Price:</p>
+                <p className="text-[1rem] font-[600]">{price}</p>
+            </div>
         </div>
     )
 }
-
 
 SolutionCard.propTypes = {
     logo: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired
+    price: PropTypes.string.isRequired,
+    className: PropTypes.string
 }
 
 export default SolutionCard
--- src/modules/public-user/stPublic/sections/SolutionsSection.jsx
+++ src/modules/public-user/stPublic/sections/SolutionsSection.jsx
@@ -6,16 +6,16 @@ import CCLogo from '../../../../assets/cc-logo.png'
 const SolutionsSection = () => {
 
     return (
-        <div className="flex items-center w-full h-[100vh]">
-            <div className="w-[80%] h-[100%] mx-auto flex flex-col items-center justify-between">
-                <div className="flex flex-col items-center w-[100%] h-[10%]">
+        <div className="flex items-center w-full h-[90vh]">
+            <div className="w-[80%] h-[75%] mx-auto flex flex-col justify-between">
+                <div className="flex flex-col items-center w-[100%]">
                     <h1>Our solutions</h1>
                     <p>Create your account quickly with just your email or social media login, then explore a wide range </p>
                 </div>
-                <div className="flex justify-between w-[100%] h-[85%]">
+                <div className="flex justify-between w-[100%] h-[65%]">
                     <SolutionCard logo={SDLogo} title="Software Development" description="Unlock your potential with comprehensive training in modern software development, from coding fundamentals to building complex applications.
 " price="$350" />
-                    <SolutionCard logo={DSLogo} title="Data Science Mastery" description="Equip yourself with the skills to analyze, interpret, and leverage data, becoming an expert in machine learning, AI, and data-driven decision-making." price="$300" />
+                    <SolutionCard logo={DSLogo} title="Data Science Mastery" description="Equip yourself with the skills to analyze, interpret, and leverage data, becoming an expert in machine learning, AI, and data-driven decision-making." price="$300"  />
                     <SolutionCard logo={CCLogo} title="Cloud Computing Expertise" description="Gain hands-on experience in cloud architecture and deployment, preparing you to design, implement, and manage scalable cloud solutions in the real world." price="$300" />
                 </div>
             </div>

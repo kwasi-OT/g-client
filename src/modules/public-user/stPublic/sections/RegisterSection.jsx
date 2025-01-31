9714f9710e456f40397f806264d63c9c61f45581
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2025-01-31 13:58:34 +0000

    created steps and register components

--- src/modules/public-user/stPublic/sections/RegisterSection.jsx
+++ src/modules/public-user/stPublic/sections/RegisterSection.jsx
@@ -1,10 +1,13 @@
-// import React from 'react'
+import Register from '../components/Register'
+import Steps from '../components/Steps'
 
 const RegisterSection = () => {
     return (
         <div className='w-full h-[100vh] bg-bg-body'>
-            <div></div>
-            <div></div>
+            <div className='w-[80%] h-full mx-auto flex justify-between items-center h-full'>
+                <Steps />
+                <Register />
+            </div>
         </div>
     )
 }

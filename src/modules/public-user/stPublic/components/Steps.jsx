5a882dfe48edb16d85d7314bd6edba53546e430e
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2025-02-01 20:24:29 +0000

    built homepage registration form

--- src/modules/public-user/stPublic/components/Steps.jsx
+++ src/modules/public-user/stPublic/components/Steps.jsx
@@ -2,8 +2,11 @@
 
 const Steps = () => {
     return (
-        <div>
-            Steps
+        <div className="w-[30%] h-[100%] flex items-center justify-center">
+            <div className="w-[90%] h-[90%] flex items-center justify-center mx-auto border border-[red]">
+                <div className="h-[100%] border border-[var(--primary-blue)]"></div>
+                <div className="w-[50%] h-[100%] border border-[var(--bg-primary-blue)]"></div>
+            </div>
         </div>
     )
 }
--- src/modules/public-user/stPublic/sections/RegisterSection.jsx
+++ src/modules/public-user/stPublic/sections/RegisterSection.jsx
@@ -3,8 +3,8 @@ import Steps from '../components/Steps'
 
 const RegisterSection = () => {
     return (
-        <div className='w-full h-[100vh] bg-bg-body'>
-            <div className='w-[80%] h-full mx-auto flex justify-between items-center h-full'>
+        <div className='w-full h-[130vh] bg-bg-body flex flex-col items-center justify-center'>
+            <div className='w-[80%] h-full mx-auto flex justify-between items-center'>
                 <Steps />
                 <Register />
             </div>

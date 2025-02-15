3355985f9fc1a201aa390d35dc523f7a861a5414
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2025-02-15 20:43:40 +0000

    added metrics section

--- src/modules/public-user/adPublic/pages/InstructorHome.jsx
+++ src/modules/public-user/adPublic/pages/InstructorHome.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../sections/HeroSection"
 import ReasonSection from "../sections/ReasonSection"
+import MetricSection from "../sections/MetricSection"
 
 const InstructorHome = () => {
     return (
         <div className='w-full h-fit flex flex-col'>
             <HeroSection />
             <ReasonSection />
+            <MetricSection />
         </div>
     )
 }
---
+++ src/modules/public-user/adPublic/sections/MetricSection.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+const MetricSection = () => {
+    return (
+        <div className="w-full h-[10.5rem] bg-[var(--primary-blue)] pt-[2%] pb-[3%] flex flex-col items-center justify-center">
+            <div className="w-[80%] h-[100%] flex items-center justify-center gap-[10%]">
+                <div>
+                    <h2 className='text-[var(--bg-white)] text-[2rem]'>100K+</h2>
+                    <p className='text-[var(--bg-white)]'>Students</p>
+                </div>
+                <div>
+                    <h2 className='text-[var(--bg-white)] text-[2rem]'>100K+</h2>
+                    <p className='text-[var(--bg-white)]'>Students</p>
+                </div>
+                <div>
+                    <h2 className='text-[var(--bg-white)] text-[2rem]'>100K+</h2>
+                    <p className='text-[var(--bg-white)]'>Students</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MetricSection

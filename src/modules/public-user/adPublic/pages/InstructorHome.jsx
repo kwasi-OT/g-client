af82d69b64e9f8a0f7edb8686a6d5e026fcd5702
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2025-02-15 19:52:48 +0000

    added instructor hero section

--- src/modules/public-user/adPublic/pages/InstructorHome.jsx
+++ src/modules/public-user/adPublic/pages/InstructorHome.jsx
@@ -1,10 +1,10 @@
-// import Header from '../../stPublic/components/Header'
+import HeroSection from "../sections/HeroSection"
 // import Footer from '../../stPublic/components/Footer'
 
 const InstructorHome = () => {
     return (
         <div className='w-full h-[100vh] flex flex-col'>
-            <h1>Instructor Home Page</h1>
+            <HeroSection />
         </div>
     )
 }
---
+++ src/modules/public-user/adPublic/sections/HeroSection.jsx
@@ -0,0 +1,20 @@
+import HeroImage from '../../../../assets/heroimg-nobg.png'
+
+const HeroSection = () => {
+    return (
+        <div className='w-full h-[37.5rem] bg-[var(--inst-hero-bg)]'>
+            <div className="w-[80%] h-[100%] mx-auto flex items-center justify-center">
+                <div className="flex flex-col justify-start items-start w-[30%] h-[90%] pt-[5%] text-[var(--inst-hero-text-h1)]">
+                    <h1 className='text-[3rem] font-bold'>Come teach <br/>with us</h1>
+                    <p className='text-[1.2rem] mt-4'>Become an instructor and change lives <br/>— including your own</p>
+                    <button className='bg-[var(--primary-blue)] text-white px-[4rem] py-[1.5rem] rounded-[0.3rem] mt-[1.5rem] hover:bg-[var(--logo-blue)] box-border'>Get Started</button>
+                </div>
+                <div className="w-[50%] h-[100%]">
+                    <img src={HeroImage} alt="Hero Image" className="w-[100%] h-[90%] object-contain" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection

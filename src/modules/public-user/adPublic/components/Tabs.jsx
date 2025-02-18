1db61f52f0e28c33246f4e28cb6d9bd0df6c3a5f
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2025-02-18 00:49:49 +0000

    fixed tabs and auth page image

--- src/modules/public-user/adPublic/components/Tabs.jsx
+++ src/modules/public-user/adPublic/components/Tabs.jsx
@@ -13,10 +13,10 @@ const Tabs = () => {
 
     return (
         <div>
-            <div className="tabs flex items-center justify-center">
-                <button onClick={() => handleTabChange('plan')} className={activeTab === 'plan' ? 'active bg-[white] text-[black] font-[600]' : 'bg-[white] text-[var(--inst-hero-text-p)]'}>Plan your curriculum</button>
-                <button onClick={() => handleTabChange('record')} className={activeTab === 'record' ? 'active bg-[white] text-[black] font-[600]' : 'bg-[white] text-[var(--inst-hero-text-p)]'}>Record your video</button>
-                <button onClick={() => handleTabChange('launch')} className={activeTab === 'launch' ? 'active bg-[white] text-[black] font-[600]' : 'bg-[white] text-[var(--inst-hero-text-p)]'}>Launch your course</button>
+            <div className="tabs flex items-center justify-center gap-[2rem]">
+                <button onClick={() => handleTabChange('plan')} className={activeTab === 'plan' ? 'active bg-[white] text-[black] font-[700]' : 'bg-[white] text-[var(--inst-hero-text-p)]'}>Plan your curriculum</button>
+                <button onClick={() => handleTabChange('record')} className={activeTab === 'record' ? 'active bg-[white] text-[black] font-[700]' : 'bg-[white] text-[var(--inst-hero-text-p)]'}>Record your video</button>
+                <button onClick={() => handleTabChange('launch')} className={activeTab === 'launch' ? 'active bg-[white] text-[black] font-[700]' : 'bg-[white] text-[var(--inst-hero-text-p)]'}>Launch your course</button>
             </div>
             <div className="w-full h-[1px] bg-[var(--primary-grey)]"></div>
             {activeTab === 'plan' && <PlanCurriculum />}
--- src/modules/public-user/adPublic/pages/InstAuth.jsx
+++ src/modules/public-user/adPublic/pages/InstAuth.jsx
@@ -1,24 +1,28 @@
+import { Link } from 'react-router-dom'
 import Logo from '../../../../assets/footer-logo.png'
 import LeftSectionImg from '../../../../assets/left-section-down.png'
 import WavesImg from '../../../../assets/waves.svg'
+import { ROUTES } from '../../../../routing/routes'
 
 const InstAuth = () => {
     return (
         <div className='w-full h-[100vh] flex'>
             <div className="authLeft w-[35%] h-full bg-[var(--primary-blue)]">
-                <div className="top w-[100%] h-[45%] flex flex-col items-center justify-center">
+                <div className="top w-[100%] h-[40%] flex flex-col items-center justify-center">
                     <div className='w-[80%] h-[80%] mx-auto flex flex-col items-start'>
-                        <img src={Logo} alt="Logo" className="w-[50%] h-[16%] object-contain" />
-                        <h1 className="text-[2rem] font-bold text-[white] pl-[0.5rem]">
+                        <Link to={ROUTES.COMMON.INSTRUCTOR} className='w-[50%] h-[17%]'>
+                            <img src={Logo} alt="Logo" className='w-[100%] h-[100%] ml-[0.8rem]'/>
+                        </Link>
+                        <h1 className="text-[2rem] font-bold text-[white] ml-[0.8rem]">
                             Create Your Account to Manage and Access the Dashboard Effortlessly.
                         </h1>
                     </div>
                 </div>
-                <div className="middle w-[100%] h-[30%] absolute top-[30%] right-[33%]">
+                <div className="middle w-[100%] h-[30%] absolute top-[21%] right-[35%]">
                     <img src={WavesImg} alt="waves image" className="w-[100%] h-[100%] object-contain" />
                 </div>
                 <div className="bottom w-[100%] h-[65%] overflow-hidden">
-                    <img src={LeftSectionImg} alt="left section image" className="w-[100%] h-[100%] object-contain overflow-clip" />
+                    <img src={LeftSectionImg} alt="left section image" className="w-[100%] h-[100%] object-cover overflow-clip" />
                 </div>
             </div>
             <div className="authRight w-[65%] h-[100%] bg-[var(--bg-white)]"></div>

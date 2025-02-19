0f51361e1a9a8dca128a0009d97d0462504509c4
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2025-02-19 13:18:47 +0000

    adjusted signup page style

--- src/modules/public-user/adPublic/components/InstSignup.jsx
+++ src/modules/public-user/adPublic/components/InstSignup.jsx
@@ -2,44 +2,78 @@ import { useForm } from "react-hook-form"
 import EmailIcon from '../../../../assets/icons/email.svg';
 import PasswordIcon from '../../../../assets/icons/lock.svg';
 import { MdChevronRight } from "react-icons/md";
+import { HiOutlineUser } from "react-icons/hi";
+import { MdError } from "react-icons/md";
 
 const InstSignup = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
     return (
-        <div className="w-[60%] h-[30%] flex flex-col gap-[1rem]">
+        <div className="w-[70%] h-[30%] flex flex-col gap-[1rem] mt-[-2rem]">
             <h1 className="text-[2rem] font-[700] leading-[3rem]">Register to get started</h1>
-            <form onSubmit={handleSubmit((data) => console.log(data))}>
-                <div className="form-container w-full flex flex-col items-center justify-center gap-[1rem]">
-                    <div className="names w-full flex items-center justify-center gap-[1rem]">
-                        <div className="firstname w-full">
-                            <input {...register("firstname", { required: true })} placeholder="First Name" />
-                            {errors.firstname && <p>First Name is required</p>}
+            <form onSubmit={handleSubmit((data) => console.log(data))} className="w-full ">
+                <div className="form-container w-full flex flex-col items-center justify-center gap-[2rem]">
+                    <div className="names w-[95%] flex items-center justify-between ">
+                        <div className="firstname w-[45%] h-[3rem] flex flex-col">
+                            <div className="firstname w-full flex items-center gap-[0.5rem] bg-[var(--input-bg)] border-b-[1px] border-[var(--primary-blue)] rounded-t-[0.3rem] p-[0.5rem]">
+                                <HiOutlineUser color="#3f3f3f" size={22}/>
+                                <input 
+                                    {...register("firstname", { required: true })} placeholder="First Name"
+                                    className="w-[90%] bg-transparent border-none outline-none text-[1rem] text-[var(--text-grey)]"
+                                />  
+                                {errors.firstname && <MdError color="var(--primary-red)" size={22}/>}
+                            </div>
+                            {errors.firstname && <p className='error-message text-[0.7rem] text-[var(--primary-red)]'>First Name is required</p>}
                         </div>
-                        <div className="lastname w-full">
-                            <input {...register("lastname", { required: true })} placeholder="Last Name" />
-                            {errors.lastname && <p>Last Name is required</p>}
+                        <div className="lastname w-[45%] h-[3rem] flex flex-col">
+                            <div className="lastname w-full flex items-center gap-[0.5rem] bg-[var(--input-bg)] border-b-[1px] border-[var(--primary-blue)] rounded-t-[0.3rem] p-[0.5rem]">
+                                <HiOutlineUser color="#3f3f3f" size={22}/>
+                                <input 
+                                    {...register("lastname", { required: true })} placeholder="Last Name"
+                                    className="w-[90%] bg-transparent border-none outline-none text-[1rem] text-[var(--text-grey)]"
+                                />
+                                {errors.lastname && <MdError color="var(--primary-red)" size={22}/>}
+                            </div>
+                            {errors.lastname && <p className='error-message text-[0.7rem] text-[var(--primary-red)]'>Last Name is required</p>}
                         </div>
                     </div>
-                    <div className="email w-full">
-                        <div className="email-field w-full flex items-center justify-center gap-[1rem]">
+                    <div className="w-[95%] h-[3rem] flex flex-col">
+                        <div className="email w-full flex items-center gap-[0.5rem] bg-[var(--input-bg)] border-b-[1px] border-[var(--primary-blue)] rounded-t-[0.3rem] p-[0.5rem]">
                             <img src={EmailIcon} alt="email" />
-                            <input {...register("email", { required: true })} placeholder="Email" />
+                            <input 
+                                {...register("email", { required: true })} 
+                                placeholder="Email"
+                                className="w-full bg-transparent border-none outline-none text-[1rem] text-[var(--text-grey)]"
+                            />
+                            {errors.email && <MdError color="var(--primary-red)" size={22}/>}
                         </div>
-                        {errors.email && <p>Email is required</p>}
+                        {errors.email && <p className='error-message text-[0.7rem] text-[var(--primary-red)]'>Email is required</p>}
                     </div>
-                    <div className="password w-full flex items-center justify-center gap-[1rem]">
-                        <div className="password-field w-full flex items-center justify-center gap-[1rem]">
-                            <img src={PasswordIcon} alt="password" />
-                            <input {...register("password", { required: true })} placeholder="Password" />
-                            {errors.password && <p>Password is required</p>}
+                    <div className="password w-[95%] flex items-center justify-between gap-[0.5rem]">
+                        <div className="w-[45%] h-[3rem] flex flex-col">
+                            <div className="password-field w-full flex items-center gap-[0.5rem] bg-[var(--input-bg)] border-b-[1px] border-[var(--primary-blue)] rounded-t-[0.3rem] p-[0.5rem]">
+                                <img src={PasswordIcon} alt="password" />
+                                <input 
+                                    {...register("password", { required: true })} placeholder="Password"
+                                    className="w-[90%] bg-transparent border-none outline-none text-[1rem] text-[var(--text-grey)]"
+                                />
+                                {errors.password && <MdError color="var(--primary-red)" size={22}/>}
+                            </div>
+                            {errors.password && <p className='error-message text-[0.7rem] text-[var(--primary-red)]'>Password is required</p>}
                         </div>
-                        <div className="confirm-password-field w-full flex items-center justify-center gap-[1rem]">
-                            <img src={PasswordIcon} alt="confirm password" />
-                            <input {...register("confirmPassword", { required: true })} placeholder="Confirm Password" />
-                            {errors.confirmPassword && <p>Confirm Password is required</p>}
+                        <div className="w-[45%] h-[3rem] flex flex-col">
+                            <div className="confirm-password-field w-full flex items-center gap-[0.5rem] bg-[var(--input-bg)] border-b-[1px] border-[var(--primary-blue)] rounded-t-[0.3rem] p-[0.5rem]">
+                                <img src={PasswordIcon} alt="confirm password" />
+                                <input 
+                                    {...register("confirmPassword", { required: true })} placeholder="Confirm Password"
+                                    className="w-[90%] bg-transparent border-none outline-none text-[1rem] text-[var(--text-grey)]"
+                                />
+                                {errors.confirmPassword && <MdError color="var(--primary-red)" size={22}/>}
+                            </div>
+                            {errors.confirmPassword && <p className='error-message text-[0.7rem] text-[var(--primary-red)]'>Confirm Password is required</p>}
                         </div>
                     </div>
-                    <button type="submit" className="w-full h-[3rem] rounded-[0.3rem] bg-[var(--primary-blue)] hover:bg-[var(--logo-blue)] text-[var(--bg-white)] flex items-center justify-center gap-[0.5rem]">
+                    <button type="submit" className="w-full h-[3rem] mt-[1rem] rounded-[0.3rem] bg-[var(--primary-blue)] hover:bg-[var(--logo-blue)] text-[var(--bg-white)] flex items-center justify-center gap-[0.5rem]">
                         Create account
                         <MdChevronRight color="var(--bg-white)" size={25} />
                     </button>
